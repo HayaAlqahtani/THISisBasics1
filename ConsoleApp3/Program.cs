0c8f1e0350ea90540624143106cb82a2812f0164
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-14 13:48:42 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "haya";
+int age = 21;
+double phone = 30.30;
+bool status = false;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(phone);
+Console.WriteLine(status);
+
+double result = Convert.ToDouble(age);
+Console.WriteLine(result);
+/////task2
+var student_name = "haya alqahtani";
+char student_Grade = 'A';
+var fav_subject = "math";
+
+Console.WriteLine("my name is " + student_name);
+Console.WriteLine("My Grade Is " + student_Grade);
+Console.WriteLine("My Favorite Subject is " + fav_subject);
+
+var height = 8;
+var width = 6;
+Console.WriteLine("the area of the rectangle is " + height * width);
